8e75bc463fb850359c7a65fb3d8bb039d22b9feb
viveksharmaui <sharma_vivek62@yahoo.com>
2019-09-12 23:51:31 +0500

    parsed and loaded from env sub folder

--- test/unit/dotenv.flow.test.js
+++ test/unit/dotenv.flow.test.js
@@ -1,9 +1,39 @@
-const dotenvFlow = require('../../lib/dotenv-flow');
+const dotenvFlow = require("../../lib/dotenv-flow");
 
-
-test('getting dotenvFlow list in .env subfolder', async () => {
-    const list = dotenvFlow.listDotenvFiles('/path/to/project', { node_env: "test" });
-    expect(list).toEqual(["D:\\path\\to\\project\\.env\\.env",
-        "D:\\path\\to\\project\\.env\\.env.test",
-        "D:\\path\\to\\project\\.env\\.env.test.local"])
+test("getting dotenvFlow list in .env subfolder", () => {
+  const list = dotenvFlow.listDotenvFiles("/path/to/project", {
+    node_env: "test"
+  });
+  expect(list).toEqual([
+    "D:\\path\\to\\project\\.env\\.env",
+    "D:\\path\\to\\project\\.env\\.env.local",
+    "D:\\path\\to\\project\\.env\\test\\.env.test",
+    "D:\\path\\to\\project\\.env\\test\\.env.test.local"
+  ]);
 });
+
+
+test("parsing file", () => {
+  const parsed = dotenvFlow.parse([
+    "D:\\path\\to\\project\\.env\\.env",
+    "D:\\path\\to\\project\\.env\\.env.local",
+    "D:\\path\\to\\project\\.env\\test\\.env.test",
+    "D:\\path\\to\\project\\.env\\test\\.env.test.local"
+  ]);
+  expect(parsed).toEqual({
+    ENV: 'ok',
+    ENV_LOCAL: 'ok',
+    ENV_TEST: 'ok',
+    ENV_TEST_LOCAL: 'ok'
+  });
+})
+
+test("loading parsed variables in process.env", () => {
+  const loaded = dotenvFlow.load([
+    "D:\\path\\to\\project\\.env\\.env",
+    "D:\\path\\to\\project\\.env\\.env.local",
+    "D:\\path\\to\\project\\.env\\test\\.env.test",
+    "D:\\path\\to\\project\\.env\\test\\.env.test.local"
+  ]);
+  expect(process.env).toMatchObject(loaded.parsed)
+})
