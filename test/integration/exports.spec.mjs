ce6acb600344eacbe987930e761d0bdcb8b6a7e6
Dan Kerimdzhanov <kerimdzhanov@gmail.com>
2023-09-14 20:43:22 +0100

    feat(dotenv-flow): add debug messaging and error warning
    This PR adds informative (yet secure) debug messages and error warnings which adds more clarity on what's happening under the dotenv-flow's hood in certain scenarios, depending on the given environment, and set of options.
    
    - debug messages can be enabled via `options.debug`,
    - while warnings are enabled by default and can be disabled via `options.silent`.

--- test/integration/exports.spec.mjs
+++ test/integration/exports.spec.mjs
@@ -1,9 +1,37 @@
 import { createRequire } from 'node:module';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
-const require = createRequire(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
 
 describe('exports', () => {
+  let _processEnvBackup;
+
+  before('backup the original `process.env` object', () => {
+    _processEnvBackup = process.env;
+  });
+
+  beforeEach('setup the `process.env` copy', () => {
+    process.env = { ..._processEnvBackup };
+  });
+
+  after('restore the original `process.env` object', () => {
+    process.env = _processEnvBackup;
+  });
+
+  beforeEach('stub `process.cwd()`', () => {
+    sinon.stub(process, 'cwd')
+      .returns(resolve(__dirname, 'fixtures', 'env'));
+  });
+
+  afterEach('restore `process.cwd()`', () => {
+    process.cwd.restore();
+  });
+
   describe('commonjs', () => {
     it('should load module using require', () => {
       const dotenv = require('../..')
